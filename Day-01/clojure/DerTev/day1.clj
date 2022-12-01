8bacc82a5f55fd80674f7f726d18138eb9106850
Tim <77175275+DerTev@users.noreply.github.com>
2022-12-01 18:18:38 +0100

    Better part two

--- Day-01/clojure/DerTev/day1.clj
+++ Day-01/clojure/DerTev/day1.clj
@@ -1,18 +1,17 @@
 (ns day1
   (:require [clojure.string :as string]))
 
-(defn get-elves-calories []
+(defn get-elve-calories []
   (map #(reduce + %) (map (partial map read-string)
                           (map #(string/split % #"\r\n")
                                (-> (slurp "input.txt")
                                    (string/split #"\r\n\r\n"))))))
 
 (defn part-one []
-  (apply max (get-elves-calories)))
+  (apply max (get-elve-calories)))
 
 (defn part-two []
-  (let [sorted (sort > (get-elves-calories))]
-    (+ (nth sorted 0) (nth sorted 1) (nth sorted 2))))
+  (reduce + (take 3 (sort > (get-elve-calories)))))
 
 (println (str "Part one: " (part-one)))
 (println (str "Part two: " (part-two)))
