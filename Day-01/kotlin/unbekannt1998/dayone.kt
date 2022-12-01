d12676dda025cb18a4bfc2f02cf877810e45679f
unbekannt <unbekannt-gitlab@protonmail.com>
2022-12-01 13:05:21 +0100

    Day-01

---
+++ Day-01/kotlin/unbekannt1998/dayone.kt
@@ -0,0 +1,33 @@
+package unbekannt1998
+
+import java.io.File
+
+val input = File("input.txt")
+
+fun main() {
+    val elves = mutableListOf<Elf>()
+    var currentElf = Elf()
+    input.readLines().forEachIndexed { indexLine, line ->
+        if (line.isBlank()) {
+            elves.add(currentElf)
+            currentElf = Elf()
+            return@forEachIndexed
+        }
+        currentElf.calories += line.toInt()
+    }
+    elves.sortByDescending { it.calories }
+    print("Most Calories on Single Elfe:")
+    println(elves.first().calories)
+
+    print("Calories Top3 Elves:")
+    var topCalories = 0
+    var elfCounter = 0
+    while (elfCounter < 3){
+        topCalories += elves[elfCounter].calories
+        elfCounter++
+    }
+    println(topCalories)
+}
+
+
+data class Elf(var calories: Int = 0)
