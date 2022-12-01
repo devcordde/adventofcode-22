caa968f1096d9f0a655b6da8a092761cb82d03d3
Lilly Tempest <46890129+RainbowDashLabs@users.noreply.github.com>
2022-12-01 20:52:30 +0100

    Update and rename Day-01/java/B33fb0n3.java to Day-01/java/B33fb0n3/Day1.java

--- Day-01/java/B33fb0n3/Day1.java
+++ Day-01/java/B33fb0n3/Day1.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.*;
 
-public class MyClass {
+public class Day1 {
     public static void main(String args[]) {
         Scanner s = new Scanner(System.in);
         s.useDelimiter("\n\n");
