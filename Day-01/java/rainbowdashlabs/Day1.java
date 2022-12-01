a09fb338b7b676cf63d0ee67f3d8782a96271cdd
Florian Fülling <46890129+RainbowDashLabs@users.noreply.github.com>
2022-12-01 16:02:18 +0100

    Add java

---
+++ Day-01/java/rainbowdashlabs/Day1.java
@@ -0,0 +1,25 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Day1 {
+    public static void main(String[] args) throws IOException {
+        var calories = Arrays.stream(Files.readString(Path.of("input.txt")).split("\n\n"))
+                             .map(group -> Arrays.stream(group.split("\n"))
+                                                 .mapToInt(Integer::parseInt)
+                                                 .sum())
+                             .toList();
+        System.out.printf("Part 1: %d%n", calories.stream()
+                                                  .mapToInt(i -> i)
+                                                  .max()
+                                                  .getAsInt());
+
+        System.out.printf("Part 2: %d%n", calories.stream()
+                                                  .sorted(Comparator.reverseOrder())
+                                                  .limit(3)
+                                                  .mapToInt(i -> i)
+                                                  .sum());
+    }
+}
