d5c98af1530d07ada36c8b6e8b423473e0647319
Lilly Tempest <46890129+RainbowDashLabs@users.noreply.github.com>
2022-12-02 14:32:40 +0100

    Inline stuff (#35)

--- Day-02/python/rainbowdashlabs/day_2_clean.py
+++ Day-02/python/rainbowdashlabs/day_2_clean.py
@@ -1,13 +1,9 @@
 win, loose = {3: 1, 2: 3, 1: 2}, {1: 3, 3: 2, 2: 1}
-choices = [[ord(e.strip().split(" ")[0]) - 64, ord(e.strip().split(" ")[1]) - 87] for e in open('input.txt') if e]
+choices = [[ord(e.strip().split(" ")[0]) - (ord('A') - 1), ord(e.strip().split(" ")[1]) - (ord('X') - 1)] for e in open('input.txt') if e]
 
 
 def check(other: int, my: int) -> int:
-    if not other - my:
-        return 3 + my
-    if win[other] == my:
-        return 6 + my
-    return my
+    return 3 + my if not other - my else 6 + my if win[other] == my else my
 
 
 print(f"Part 1 {sum([check(*e) for e in choices])}")
