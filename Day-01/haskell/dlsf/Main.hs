59b39bb22386dfecff5cde7a2131a21a167f7aff
das_ <git@heap.mozmail.com>
2022-12-04 22:51:09 +0100

    Improve Haskell solution for Day 01

--- Day-01/haskell/dlsf/Main.hs
+++ Day-01/haskell/dlsf/Main.hs
@@ -7,6 +7,6 @@ import Text.Printf
 main :: IO ()
 main = do
     input <- readFile "input.txt"
-    let sorted = sort [d | a <- splitOn "\n\n" input, let b = splitOn "\n" a, let c = map (read :: String -> Int) b, let d = sum c]
+    let sorted = sort [calories | elve <- splitOn "\n\n" input, let calories = sum $ map (read :: String -> Int) $ splitOn "\n" elve]
     printf "%s %d\n" "Part 1:" $ last sorted
     printf "%s %d\n" "Part 2:" $ sum $ take 3 $ reverse sorted
