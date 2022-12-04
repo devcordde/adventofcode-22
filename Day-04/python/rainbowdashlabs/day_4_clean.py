d8d4e503a87edf9128c0e1ec0a28b89c0a789c9f
Lilly Tempest <46890129+RainbowDashLabs@users.noreply.github.com>
2022-12-04 11:40:03 +0100

    Day 4 (#72)
    * Setup day 4
    
    * Python day 4
    
    * Fix file names

--- Day-04/python/rainbowdashlabs/day_4_clean.py
+++ Day-04/python/rainbowdashlabs/day_4_clean.py
@@ -1,5 +1,12 @@
-sections = [[set(range(int(a[0]), int(a[1]) + 1)) for a in [x.split("-") for x in e]] for e in [e.strip().split(",") for e in open("input.txt")]]
+sections = [e.strip().split(",") for e in open("input.txt")]
 
-print(f"Part 1: {len([e for e in sections if e[1].issubset(e[0]) or e[0].issubset(e[1])])}")
+sections = [[a.split("-") for a in e] for e in sections]
 
-print(f"Part 2: {len([e for e in sections if e[0].intersection(e[1])])}")
+sections = [[set(range(int(a[0]), int(a[1]) + 1)) for a in e] for e in sections]
+
+subsets = [e for e in sections if e[1].issubset(e[0]) or e[0].issubset(e[1])]
+
+print(f"Part 1: {len(subsets)}")
+
+intersections = [e for e in sections if e[0].intersection(e[1])]
+print(f"Part 2: {len(intersections)}")
