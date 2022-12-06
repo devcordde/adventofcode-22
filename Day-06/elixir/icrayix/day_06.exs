10d1b4ccdcfc3e4f7a08d3277fa4badd3221b165
icrayix <icrayix@gmail.com>
2022-12-06 19:37:22 +0100

    Extract to function

--- Day-06/elixir/icrayix/day_06.exs
+++ Day-06/elixir/icrayix/day_06.exs
@@ -2,26 +2,21 @@ input = File.read!("input.txt") |> String.trim() |> String.to_charlist()
 
 has_uniq_chars = &(length(Enum.uniq(&1)) == length(&1))
 
+get_marker_index =
+  &(Enum.reduce_while(input, [], fn x, acc ->
+      # check if havent looped for at least 4 chars and check if the last 4 chars are not unique
+      if length(acc) < 4 or not (Enum.take(acc, &1) |> has_uniq_chars.()) do
+        {:cont, acc ++ [x]}
+      else
+        {:halt, acc}
+      end
+    end)
+    |> length())
+
 # part 1
-Enum.reduce_while(input, [], fn x, acc ->
-  # check if havent looped for at least 4 chars and check if the last 4 chars are not unique
-  if length(acc) < 4 or not (Enum.take(acc, -4) |> has_uniq_chars.()) do
-    {:cont, acc ++ [x]}
-  else
-    {:halt, acc}
-  end
-end)
-|> length()
+get_marker_index.(-4)
 |> IO.puts()
 
 # part 2
-Enum.reduce_while(input, [], fn x, acc ->
-  # check if havent looped for at least 14 chars and check if the last 4 chars are not unique
-  if length(acc) < 4 or not (Enum.take(acc, -14) |> has_uniq_chars.()) do
-    {:cont, acc ++ [x]}
-  else
-    {:halt, acc}
-  end
-end)
-|> length()
+get_marker_index.(-14)
 |> IO.puts()
