bafb51cf790f5edab04b295c57ce2db3dcae4cbc
JohnnyJayJay <johnnyjayjay02@gmail.com>
2022-12-08 22:44:44 +0100

    Add java solutions for day 6 and 7

---
+++ Day-06/java/B33fb0n3/Day6_1.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Day6_1 {
+    public static void main(String args[]) {
+        Scanner s = new Scanner(System.in);
+        List<String> letters = new LinkedList<String>(Arrays.asList(s.nextLine().split("")));
+        for(int i = 0; i < letters.size(); i++) {
+            int start = 4;
+            if(i < start)
+                continue;
+
+            List tempCheck = letters.subList(0 + (i - start), start + (i-start));
+            Set<String> set = new HashSet<String>(tempCheck);
+
+            if(set.size() < tempCheck.size()){
+            } else {
+                System.out.println("Result: "+i);
+                break;
+            }
+        }
+    }
+}
