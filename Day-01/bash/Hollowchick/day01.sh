cc13941b7e8f32027a4a78a1e1a880d3ebabb660
Jamarley <funkfinkel@gmail.com>
2022-12-09 19:02:38 +0100

    Update day01.sh

--- Day-01/bash/Hollowchick/day01.sh
+++ Day-01/bash/Hollowchick/day01.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
 calories=0
-countarr=0
 array=()
 num1=0;num2=0;num3=0
 while read -r line; do
 	if [ "$line" != "" ]; then
 		calories=$((calories+$line))
 	else
-		array+="$calories "; calories=0; countarr+=1
+		array+="$calories "; calories=0
 	fi
 done <$"input"
 for i in ${array[@]}; do
