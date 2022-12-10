8078006c6754d6e9144f7cc81b4be680596d6331
Kinamura <airfighter001@users.noreply.github.com>
2022-12-10 22:24:09 +0100

    Solution Day-09 + Day-10 C (#128)
    * Solution Day-09 C
    
    * Solution Day-10 C
    
    * Shorter Solution Day-10 C
    
    Some cleanup, removed global variables, unused include
    
    * Fixed Indentation

---
+++ Day-10/c/Kinamura/Day-10-short.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void draw(int position, int cRegister, int cycle) {
+    if(cycle % 40 == 0) {fprintf(stdout, "\n");}
+    (position < cRegister +2 && position > cRegister -2) ? fprintf(stdout, "#"):fprintf(stdout, ".");
+}
+
+int checkCycle(int sStrength, int cycle, int cRegister) {
+    if( ((cycle + 21) % 40) == 0) {sStrength = sStrength + ((cycle+1) * cRegister);}
+    draw((cycle % 40), cRegister, cycle);
+    return sStrength;
+}
+
+int main() {
+    int sStrength = 0, cycle = 0, cRegister = 1, value;
+    FILE *input = fopen("Input_Day10.txt", "r");
+    if( input != NULL) {
+        char line[16], instr[8], *temp;
+        while( fgets(line, sizeof(line), input) != NULL) {
+            strcpy(instr, strtok(line, " "));
+            if( instr[0] == 'n') {
+                sStrength = checkCycle(sStrength, cycle, cRegister);
+                cycle ++;
+            } else {
+                value = strtol(strtok(NULL, " "), &temp, 10);
+                sStrength = checkCycle(sStrength, cycle, cRegister);
+                cycle++;
+                sStrength = checkCycle(sStrength, cycle, cRegister);
+                cycle++;
+                cRegister += value;
+            }
+        }
+    }
+    fprintf(stdout, "\n%d\n", sStrength);
+    return 0;
+}
