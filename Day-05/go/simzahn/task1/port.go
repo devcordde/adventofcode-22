359f1262b0c93c8dd16def9619802f8a99af64b0
Simzahn <74320612+Simzahn001@users.noreply.github.com>
2022-12-13 11:21:27 +0100

    Add Go solutions for days 1-7 (#148)
    * First six days with Go
    
    * Removed Example and Input files
    
    Made the example and input files to .txt files, which are ignored by git
    
    * Remove input file of day 1
    
    Forgot this day with the previous commit ;(
    
    * Go solution of day 7
    
    Co-authored-by: Simzahn <simon.hochenberger@chello.at>

---
+++ Day-05/go/simzahn/task1/port.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"strconv"
+)
+
+type Create struct {
+	name string
+}
+
+type Stack struct {
+	creates []Create
+}
+
+type Port struct {
+	stacks []Stack
+}
+
+func (c *Create) NewCreate(name string) {
+	c.name = name
+}
+
+func (p *Port) NewPort(input []string) {
+
+	//get the port length
+	portLength, err := strconv.Atoi(input[len(input)-1][len(input[len(input)-1])-1:]) //get the last char of the last element of the array
+	if err != nil {
+		panic(err)
+	}
+	//generate all stacks
+	for i := 0; i < portLength; i++ {
+		p.addStack()
+	}
+
+	//put data into port struct
+	for i := len(input) - 2; i >= 0; i-- {
+
+		for j := 0; 4*j+1 < len(input[i]); j++ {
+			letter := string([]rune(input[i])[4*j+1])
+			if !(letter == " ") {
+				p.GetStack(j).addCreate(Create{name: letter})
+			}
+		}
+
+	}
+
+}
+
+func (p *Port) GetPortLengh() int {
+	return len(p.stacks)
+}
+
+func (p *Port) addStack() {
+	p.stacks = append(p.stacks, Stack{creates: []Create{}})
+}
+
+func (s *Stack) addCreate(create Create) {
+	s.creates = append(s.creates, create)
+}
+
+func (s *Stack) removeCreate() Create {
+	create := s.creates[len(s.creates)-1]
+	s.creates = s.creates[:len(s.creates)-1]
+	return create
+}
+
+func (s *Stack) GetCreate(position int) Create {
+	return s.creates[position]
+}
+
+func (s *Stack) Height() int {
+	return len(s.creates)
+}
+
+func (c Create) GetName() string {
+	return c.name
+}
+
+func (p *Port) GetStack(index int) *Stack {
+	return &(p.stacks[index])
+}
+
+// View display the port as sting
+func (p *Port) View() string {
+	//@TODO add view
+	return ""
+}
+
+// MoveCreates moves the given of creates from the first stack (fromStack) to the second Stack (toStack)
+func (p *Port) MoveCreates(amount int, fromStack int, toStack int) {
+	movedCreates := []Create{}
+	for i := 0; i < amount; i++ {
+		movedCreates = append(movedCreates, p.GetStack(fromStack).removeCreate())
+	}
+	for i := 0; i < len(movedCreates); i++ {
+		p.GetStack(toStack).addCreate(movedCreates[i])
+	}
+}
---
+++ Day-07/go/simzahn/task1/task1.go
@@ -0,0 +1,138 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+
+	//create the filesystem
+	var filesystem = new(Filesystem)
+	filesystem.NewFilesystem("/")
+
+	//open the filescanner for the input file
+	readFile, err := os.Open("input.txt")
+	if err != nil {
+		panic(err)
+	}
+	filescanner := bufio.NewScanner(readFile)
+
+	//read the input and put the data into the object
+	for filescanner.Scan() {
+	HOP:
+		line := filescanner.Text()
+		//if line is "" (aka last line) then break the loop
+		if line == "" {
+			break
+		}
+		com, arg := ReadCommand(line)
+
+		//handle the commands
+		switch com {
+		case "ls":
+
+			//loop until a command occurs
+			for filescanner.Scan() {
+
+				//get the current line as string and further as command
+				line = filescanner.Text()
+				com, _ = ReadCommand(line)
+
+				//if a command is found, the loop should be broken
+				if com != "" {
+					break
+				}
+
+				//create dir or file
+				if line[0:3] == "dir" {
+					//create dir
+					dir := new(Directory)
+					dir.NewDirectory(line[4:], filesystem.currentDirectory)
+					filesystem.GetCurrentDir().AddContainable(dir)
+					//file
+				} else {
+					//create the file
+					nameAndSize := strings.Split(line, " ")
+					size, err := strconv.Atoi(nameAndSize[0])
+					if err != nil {
+						panic(err)
+					}
+					file := new(File)
+					file.NewFile(nameAndSize[1], size)
+					filesystem.GetCurrentDir().AddContainable(file)
+				}
+
+			}
+			goto HOP
+
+		case "cd":
+
+			if arg == ".." {
+				filesystem.GoBack()
+			} else if arg == "/" {
+				filesystem.ToRoot()
+			} else {
+				if !filesystem.EnterDir(arg) {
+					fmt.Println("Something went wrong")
+					fmt.Println("Dir not found!")
+				}
+			}
+
+		//if the command in neither "cd" nor "ls", something went wrong
+		default:
+			panic("no valid command")
+
+		}
+
+	}
+
+	err = readFile.Close()
+	if err != nil {
+		return
+	}
+
+	var sum = 0
+
+	//get all dirs
+	allDirs := filesystem.GetRootDir().GetDirs(true)
+
+	//loop though all dirs
+	for _, currentDir := range allDirs {
+
+		//get how lage the dir is
+		dirSize := 0
+		for _, file := range currentDir.GetFiles(true) {
+			dirSize += file.GetSize()
+		}
+		//check if the dir is smaller than 100 000
+		if dirSize < 100000 {
+			sum += dirSize
+		}
+
+	}
+
+	fmt.Println(sum)
+}
+
+// ReadCommand the functions returns the command, and if there is an argument, the argument
+// ReadCommand returns an empty string ("") if the input doesn't contain a valid command
+func ReadCommand(commandInput string) (command string, arg string) {
+	command = commandInput[2:4]
+
+	if commandInput[0:1] != "$" {
+		return "", ""
+	}
+	if command != "ls" && command != "cd" {
+		return "", ""
+	}
+
+	if command == "cd" {
+		arg = commandInput[5:]
+	}
+
+	return
+}
